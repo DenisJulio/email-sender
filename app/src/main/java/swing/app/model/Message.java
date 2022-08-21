fdff42849d6abdc2a7b758355d55f114734e9605
Denis Julio <djms1234@gmail.com>
2022-08-21 12:55:39 -0300

    refactors

--- app/src/main/java/swing/app/model/Message.java
+++ app/src/main/java/swing/app/model/Message.java
@@ -8,25 +8,19 @@ import lombok.Value;
 @Value
 public class Message {
 
-	@NotNull
-	@Email
-	@NotBlank
-	private String from;
-	@Email
-	@NotNull
-	@NotBlank
-	private String to;
-	@NotNull
-	@NotBlank
-	private String subject;
-	@NotNull
-	@NotBlank
-	private String content;
+    @NotNull
+    @Email
+    @NotBlank
+    String from;
+    @Email
+    @NotNull
+    @NotBlank
+    String to;
+    @NotNull
+    @NotBlank
+    String subject;
+    @NotNull
+    @NotBlank
+    String content;
 
-	public Message(String from, String to, String subject, String content) {
-		this.from = from;
-		this.to = to;
-		this.subject = subject;
-		this.content = content;
-	}
 }
