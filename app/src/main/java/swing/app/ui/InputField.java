4e99cc105fe111c392a673b9dc6eba0aeb1d19c2
Denis Julio <djms1234@gmail.com>
2022-08-20 23:43:46 -0300

    fix bugs and refactor

--- app/src/main/java/swing/app/ui/InputField.java
+++ app/src/main/java/swing/app/ui/InputField.java
@@ -10,6 +10,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.io.Serial;
 import java.util.Optional;
 import java.util.function.Predicate;
 
@@ -38,245 +39,255 @@ import lombok.Getter;
  */
 public final class InputField extends JPanel implements DocumentListener, CaretListener {
 
-	private static final long serialVersionUID = 1L;
-	public static final int TEXT_FIELD = 0;
-	public static final int TEXT_AREA = 1;
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public static final int TEXT_FIELD = 0;
+    public static final int TEXT_AREA = 1;
 
-	private JLabel mainLabel;
-	private JLabel warnLabel;
-	@Getter
-	private JTextComponent inputTextComponent;
-	private Box labelsBox;
-	private GridBagConstraints labelsBoxGBC;
-	private GridBagConstraints inputTextComponentGBC;
-	@Getter
-	/**
-	 * Tracks the validity state of the user provided input.
-	 */
-	private boolean validInput;
+    private JLabel mainLabel;
+    private JLabel warnLabel;
+    @Getter
+    private JTextComponent inputTextComponent;
+    private Box labelsBox;
+    private GridBagConstraints labelsBoxGBC;
+    private GridBagConstraints inputTextComponentGBC;
+    @Getter
+    /**
+     * Tracks the validity state of the user provided input.
+     */
+    private boolean validInput;
 
-	private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-	private final transient Predicate<String> validationLogic;
-	
-	private InputField(String mainLabelText, String warnLabelText, Predicate<String> validationLogic,
-			int textComponentType) {
-		this.validationLogic = validationLogic;
-		this.mainLabel = createMainLabel(mainLabelText);
-		this.warnLabel = createWarnLabel(warnLabelText);
-		this.inputTextComponent = createInputTextComponent(textComponentType);
-		this.labelsBox = createLabelsBox(mainLabel, warnLabel);
-		this.labelsBoxGBC = createLabelsBoxGBC();
-		this.inputTextComponentGBC = createInputTextComponentGBC();
-		// ----------- Layout placement
-		setLayout(createLayout());
-		add(labelsBox, labelsBoxGBC);
-		if (inputTextComponent instanceof JTextArea textArea) {
-			var scroollContainer = createScrollableTextAreaContainer(textArea);
-			add(scroollContainer, inputTextComponentGBC);
-		} else {
-			add(inputTextComponent, inputTextComponentGBC);
-		}
-	}
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private final transient Predicate<String> validationLogic;
 
-	/**
-	 * Attempts to retrieve the validated user provided input.
-	 * @return An {@code Optional<String>} with the validated user input,
-	 * or an {@code Optional.empty()} if the provided input is invalid.
-	 */
-	public Optional<String> getValidatedInput() {
-		var isInputValid = this.validationLogic.test(this.inputTextComponent.getText());
-		return isInputValid 
-				? Optional.of(this.inputTextComponent.getText()) 
-				: Optional.empty();
-	}
-	
-	private void setValidInput(boolean newValue) {
-		var oldValue = this.validInput;
-		this.validInput = newValue;
-		pcs.firePropertyChange("validInput", oldValue, newValue);
-	}
-	
-	public static Builder builder() {
-		return new Builder();
-	}
-	
-	// -------------------------[Listeners]-----------------------------------
-	
-	@Override
-	public void insertUpdate(DocumentEvent e) {
-		if (nonNull(this.validationLogic)) {
-			var doc = e.getDocument();
-			String text = "";
-			try {
-				text = doc.getText(0, doc.getEndPosition().getOffset());
-			} catch (BadLocationException exception) {
-				exception.printStackTrace();
-			}
-			setValidInput(this.validationLogic.test(text));
-			warnLabel.setVisible(!isValidInput());
-		}
-	}
-	
-	@Override
-	public void removeUpdate(DocumentEvent e) {
-		insertUpdate(e);
-	}
-	
-	@Override
-	public void changedUpdate(DocumentEvent e) {
-		 insertUpdate(e);
-	}
-	
-	@Override
-	public void caretUpdate(CaretEvent e) {
-		if (nonNull(this.validationLogic)) {
-			var textComponent = (JTextComponent) e.getSource();
-			setValidInput(this.validationLogic.test(textComponent.getText()));
-			warnLabel.setVisible(!isValidInput());
-		}
-	}
-	
-	// ------------------------[Component Initialization]------------------------------
+    private InputField(String mainLabelText, String warnLabelText, Predicate<String> validationLogic,
+                       int textComponentType) {
+        this.validationLogic = validationLogic;
+        this.mainLabel = createMainLabel(mainLabelText);
+        this.warnLabel = createWarnLabel(warnLabelText);
+        this.inputTextComponent = createInputTextComponent(textComponentType);
+        this.labelsBox = createLabelsBox(mainLabel, warnLabel);
+        this.labelsBoxGBC = createLabelsBoxGBC();
+        this.inputTextComponentGBC = createInputTextComponentGBC();
+        // ----------- Layout placement
+        setLayout(createLayout());
+        add(labelsBox, labelsBoxGBC);
+        if (inputTextComponent instanceof JTextArea textArea) {
+            var scrollContainer = createScrollableTextAreaContainer(textArea);
+            add(scrollContainer, inputTextComponentGBC);
+        } else {
+            add(inputTextComponent, inputTextComponentGBC);
+        }
+    }
 
-	private JLabel createMainLabel(String mainLabelText) {
-		return new JLabel(mainLabelText);
-	}
+    // -----------------------------[Public API]----------------------------------------
 
-	private JLabel createWarnLabel(String warnLabelText) {
-		var font = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
-		var label = new JLabel();
-		label.setFont(font);
-		label.setText(warnLabelText);
-		label.setHorizontalAlignment(SwingConstants.TRAILING);
-		label.setForeground(Color.red);
-//		label.setVisible(false);
-		return label;
-	}
+    /**
+     * Attempts to retrieve the validated user provided input.
+     *
+     * @return An {@code Optional<String>} with the validated user input,
+     * or an {@code Optional.empty()} if the provided input is invalid or
+     * no validation logic is set up.
+     */
+    public Optional<String> getValidatedInput() {
+        if (nonNull(this.validationLogic)) {
+            var isInputValid = this.validationLogic.test(this.inputTextComponent.getText());
+            return isInputValid
+                    ? Optional.of(this.inputTextComponent.getText())
+                    : Optional.empty();
+        }
+        return Optional.empty();
+    }
 
-	private Box createLabelsBox(JLabel label, JLabel wLabel) {
-		var box = Box.createHorizontalBox();
-		box.add(label);
-		box.add(Box.createHorizontalGlue());
-		box.add(wLabel);
-		return box;
-	}
+    public static Builder builder() {
+        return new Builder();
+    }
 
-	private JTextField createInputField() {
-		var textField = new JTextField();
-		textField.getDocument().addDocumentListener(this);
-		textField.addCaretListener(this);
-		return textField;
-	}
+    // --------------
 
-	private JTextArea createTextAreaComponent() {
-		var textArea = new JTextArea();
-		textArea.setTabSize(4);
-		textArea.setLineWrap(true);
-		textArea.setAutoscrolls(true);
-		textArea.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		textArea.getDocument().addDocumentListener(this);
-		textArea.addCaretListener(this);
-		return textArea;
-	}
-	
-	private JScrollPane createScrollableTextAreaContainer(JTextArea textArea) {
-		var scrollPane = new JScrollPane(textArea);
-		scrollPane.setPreferredSize(new Dimension(400, 300));
-		scrollPane.setMinimumSize(new Dimension(400, 300));
-		return scrollPane;
-	}
-	
-	private JTextComponent createInputTextComponent(int textComponentType) {
-		return switch (textComponentType) {
-			case TEXT_FIELD -> createInputField();
-			case TEXT_AREA -> createTextAreaComponent();
-			default ->
-				throw new IllegalArgumentException("Unexpected value: " + textComponentType);
-		};
-	}
-	
-	// --------------------------------[Layout Manager]-------------------------------
-	
-	private GridBagLayout createLayout() {
-		var layout = new GridBagLayout();
-		layout.columnWeights = new double[] { 1.0 };
-		return layout;
-	}
-	
-	// ---------------------------[GridBagConstraints]--------------------------------
+    private void setValidInput(boolean newValue) {
+        var oldValue = this.validInput;
+        this.validInput = newValue;
+        pcs.firePropertyChange("validInput", oldValue, newValue);
+    }
 
-	private GridBagConstraints createLabelsBoxGBC() {
-		var constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.insets = new Insets(0, 0, 5, 0);
-		return constraints;
-	}
-	
-	private GridBagConstraints createInputTextComponentGBC() {
-		var constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.ipady = 5;
-		return constraints;
-	}
+    // -------------------------[Listeners]-----------------------------------
 
-	// ----------------------------[Builder]---------------------------------------------
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        if (nonNull(this.validationLogic)) {
+            var doc = e.getDocument();
+            String text = "";
+            try {
+                text = doc.getText(0, doc.getEndPosition().getOffset());
+            } catch (BadLocationException exception) {
+                exception.printStackTrace();
+            }
+            setValidInput(this.validationLogic.test(text));
+            warnLabel.setVisible(!isValidInput());
+        }
+    }
 
-	public static class Builder {
-		private String labelText;
-		private String warnLabelText;
-		private Predicate<String> validationLogic;
-		private PropertyChangeListener listener;
-		private int textComponentType = 0;
-		
-		public Builder labelText(String text) {
-			this.labelText = text;
-			return this;
-		}
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        insertUpdate(e);
+    }
 
-		public Builder warnLabelText(String text) {
-			this.warnLabelText = text;
-			return this;
-		}
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        insertUpdate(e);
+    }
 
-		/**
-		 *Logic used internally to determine weather user input is valid or not. 
-		 * @param validationLogic
-		 */
-		public Builder validateInputBy(Predicate<String> validationLogic) {
-			this.validationLogic = validationLogic;
-			return this;
-		}
-		
-		/**
-		 *A {@code PropertyChangeListener} that listens for changes in the validity
-		 *state of the provided user input.
-		 *Calls to {@code event.getNewValue()} and {@code event.getOldValue()} returns:
-		 *<ul>
-		 *	<li><b>true</b> if valid</li>
-		 *	<li><b>false</b> if invalid</li>
-		 *</ul>
-		 * @param listener
-		 */
-		public Builder addInputValidityListener(PropertyChangeListener listener) {
-			this.listener = listener;
-			return this;
-		}
-		
-		public Builder textComponentType(int textComponentType) {
-			this.textComponentType = textComponentType;
-			return this;
-		}
+    @Override
+    public void caretUpdate(CaretEvent e) {
+        if (nonNull(this.validationLogic)) {
+            var textComponent = (JTextComponent) e.getSource();
+            setValidInput(this.validationLogic.test(textComponent.getText()));
+            warnLabel.setVisible(!isValidInput());
+        }
+    }
 
-		public InputField build() {
-			var inputField = new InputField(labelText, warnLabelText, validationLogic, textComponentType);
-			inputField.pcs.addPropertyChangeListener("validInput", listener);
-			return inputField;
-		}
-	}
+    // ------------------------[Component Initialization]------------------------------
+
+    private JLabel createMainLabel(String mainLabelText) {
+        return new JLabel(mainLabelText);
+    }
+
+    private JLabel createWarnLabel(String warnLabelText) {
+        var font = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
+        var label = new JLabel();
+        label.setFont(font);
+        label.setText(warnLabelText);
+        label.setHorizontalAlignment(SwingConstants.TRAILING);
+        label.setForeground(Color.red);
+        return label;
+    }
+
+    private Box createLabelsBox(JLabel label, JLabel wLabel) {
+        var box = Box.createHorizontalBox();
+        box.add(label);
+        box.add(Box.createHorizontalGlue());
+        box.add(wLabel);
+        return box;
+    }
+
+    private JTextField createInputField() {
+        var textField = new JTextField();
+        textField.getDocument().addDocumentListener(this);
+        textField.addCaretListener(this);
+        return textField;
+    }
+
+    private JTextArea createTextAreaComponent() {
+        var textArea = new JTextArea();
+        textArea.setTabSize(4);
+        textArea.setLineWrap(true);
+        textArea.setAutoscrolls(true);
+        textArea.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        textArea.getDocument().addDocumentListener(this);
+        textArea.addCaretListener(this);
+        return textArea;
+    }
+
+    private JScrollPane createScrollableTextAreaContainer(JTextArea textArea) {
+        var scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(400, 300));
+        scrollPane.setMinimumSize(new Dimension(400, 300));
+        return scrollPane;
+    }
+
+    private JTextComponent createInputTextComponent(int textComponentType) {
+        return switch (textComponentType) {
+            case TEXT_FIELD -> createInputField();
+            case TEXT_AREA -> createTextAreaComponent();
+            default -> throw new IllegalArgumentException("Unexpected value: " + textComponentType);
+        };
+    }
+
+    // --------------------------------[Layout Manager]-------------------------------
+
+    private GridBagLayout createLayout() {
+        var layout = new GridBagLayout();
+        layout.columnWeights = new double[]{1.0};
+        return layout;
+    }
+
+    // ---------------------------[GridBagConstraints]--------------------------------
+
+    private GridBagConstraints createLabelsBoxGBC() {
+        var constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(0, 0, 5, 0);
+        return constraints;
+    }
+
+    private GridBagConstraints createInputTextComponentGBC() {
+        var constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.ipady = 5;
+        return constraints;
+    }
+
+    // ----------------------------[Builder]---------------------------------------------
+
+    public static class Builder {
+        private String labelText;
+        private String warnLabelText;
+        private Predicate<String> validationLogic;
+        private PropertyChangeListener listener;
+        private int textComponentType = 0;
+
+        public Builder labelText(String text) {
+            this.labelText = text;
+            return this;
+        }
+
+        public Builder warnLabelText(String text) {
+            this.warnLabelText = text;
+            return this;
+        }
+
+        /**
+         * Logic used internally to determine weather user input is valid or not.
+         *
+         * @param validationLogic
+         */
+        public Builder validateInputBy(Predicate<String> validationLogic) {
+            this.validationLogic = validationLogic;
+            return this;
+        }
+
+        /**
+         * A {@code PropertyChangeListener} that listens for changes in the validity
+         * state of the provided user input.
+         * Calls to {@code event.getNewValue()} and {@code event.getOldValue()} returns:
+         * <ul>
+         * 	<li><b>true</b> if valid</li>
+         * 	<li><b>false</b> if invalid</li>
+         * </ul>
+         *
+         * @param listener
+         */
+        public Builder addInputValidityListener(PropertyChangeListener listener) {
+            this.listener = listener;
+            return this;
+        }
+
+        public Builder textComponentType(int textComponentType) {
+            this.textComponentType = textComponentType;
+            return this;
+        }
+
+        public InputField build() {
+            var inputField = new InputField(labelText, warnLabelText, validationLogic, textComponentType);
+            inputField.pcs.addPropertyChangeListener("validInput", listener);
+            return inputField;
+        }
+    }
 }
--- app/src/main/java/swing/app/ui/WriteMessagePanel.java
+++ app/src/main/java/swing/app/ui/WriteMessagePanel.java
@@ -11,113 +11,113 @@ import javax.swing.JPanel;
 import swing.app.controller.WriteMessagePanelController;
 
 public class WriteMessagePanel extends JPanel {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private WriteMessagePanelController controller;
-	private InputField toInputField;
-	private InputField subjectInputField;
-	private InputField messageContentInputField;
-	private JButton sendButton;
 
-	private GridBagConstraints toInputFieldGBC;
-	private GridBagConstraints subjectInputFieldGBC;
-	private GridBagConstraints messageContentGBC;
-	private GridBagConstraints sendButtonGBC;
+    private static final long serialVersionUID = 1L;
 
-	public WriteMessagePanel(WriteMessagePanelController controller) {
-		this.controller = controller;
-		this.toInputField = createToInputField();
-		this.subjectInputField = createSubjectInputField();
-		this.messageContentInputField = createMessageContentInputField();
-		this.sendButton = createSendButton();
+    private WriteMessagePanelController controller;
+    private InputField toInputField;
+    private InputField subjectInputField;
+    private InputField messageContentInputField;
+    private JButton sendButton;
 
-		this.toInputFieldGBC = createToInputFieldGBC();
-		this.subjectInputFieldGBC = createSubjectInputFieldGBC();
-		this.messageContentGBC = createMessageContentGBC();
-		sendButtonGBC = createSendButtonGBC();
+    private GridBagConstraints toInputFieldGBC;
+    private GridBagConstraints subjectInputFieldGBC;
+    private GridBagConstraints messageContentGBC;
+    private GridBagConstraints sendButtonGBC;
 
-		setLayout(new GridBagLayout());
-		setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
-		
-		add(toInputField, toInputFieldGBC);
-		add(subjectInputField, subjectInputFieldGBC);
-		add(messageContentInputField, messageContentGBC);
-		add(sendButton, sendButtonGBC);
-	}
+    public WriteMessagePanel(WriteMessagePanelController controller) {
+        this.controller = controller;
+        this.toInputField = createToInputField();
+        this.subjectInputField = createSubjectInputField();
+        this.messageContentInputField = createMessageContentInputField();
+        this.sendButton = createSendButton();
 
-	// -------------------------------[Components]-----------------------------------------
+        this.toInputFieldGBC = createToInputFieldGBC();
+        this.subjectInputFieldGBC = createSubjectInputFieldGBC();
+        this.messageContentGBC = createMessageContentGBC();
+        this.sendButtonGBC = createSendButtonGBC();
 
-	private InputField createToInputField() {
-		return InputField.builder()
-				.labelText("To:")
-				.warnLabelText("<html>⚠ e-mail in the form: <i>example@mail.com</i></html>")
-				.validateInputBy(controller::isValidToInputField)
-				.build();
-	}
+        setLayout(new GridBagLayout());
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
 
-	private InputField createSubjectInputField() {
-		return InputField.builder()
-				.labelText("Subject:")
-				.warnLabelText("⚠ subject can't be empty")
-				.validateInputBy(controller::isValidSubjectInputField)
-				.build();
-	}
-	
-	private InputField createMessageContentInputField() {
-		return InputField.builder()
-				.labelText("Content:")
-				.warnLabelText("⚠ message content can't be empty")
-				.validateInputBy(controller::isValidContentMessage)
-				.textComponentType(InputField.TEXT_AREA)
-				.build();
-	}	
+        add(toInputField, toInputFieldGBC);
+        add(subjectInputField, subjectInputFieldGBC);
+        add(messageContentInputField, messageContentGBC);
+        add(sendButton, sendButtonGBC);
+    }
 
-	private JButton createSendButton() {
-		var button = new JButton("Send");
-		button.setEnabled(false);
-		return button;
-	}
+    // -------------------------------[Components]-----------------------------------------
 
-	// -------------------------------[GridBagConstraints]-----------------------------------
+    private InputField createToInputField() {
+        return InputField.builder()
+                .labelText("To:")
+                .warnLabelText("<html>&#9888 e-mail in the form: <i>example@mail.com</i></html>")
+                .validateInputBy(controller::isValidToInputField)
+                .build();
+    }
 
-	private GridBagConstraints createToInputFieldGBC() {
-		var constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.insets = new Insets(0, 0, 5, 0);
-		return constraints;
-	}
+    private InputField createSubjectInputField() {
+        return InputField.builder()
+                .labelText("Subject:")
+                .warnLabelText("<html>&#9888 subject can't be empty</html>")
+                .validateInputBy(controller::isValidSubjectInputField)
+                .build();
+    }
 
-	private GridBagConstraints createSubjectInputFieldGBC() {
-		var constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.insets = new Insets(0, 0, 5, 0);
-		return constraints;
-	}
+    private InputField createMessageContentInputField() {
+        return InputField.builder()
+                .labelText("Content:")
+                .warnLabelText("<html>&#9888 message content can't be empty</html>")
+                .validateInputBy(controller::isValidContentMessage)
+                .textComponentType(InputField.TEXT_AREA)
+                .build();
+    }
 
-	private GridBagConstraints createMessageContentGBC() {
-		var constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 2;
-		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.insets = new Insets(0, 0, 5, 0);
-		return constraints;
-	}
-	
-	private GridBagConstraints createSendButtonGBC() {
-		var constraints = new GridBagConstraints();
-		constraints.gridx = 0;
-		constraints.gridy = 3;
-		constraints.fill = GridBagConstraints.HORIZONTAL;
-		constraints.anchor = GridBagConstraints.NORTH;
-		constraints.weighty = 1;
-		constraints.ipady = 8;
-		return constraints;
-	}
+    private JButton createSendButton() {
+        var button = new JButton("Send");
+        button.setEnabled(false);
+        return button;
+    }
+
+    // -------------------------------[GridBagConstraints]-----------------------------------
+
+    private GridBagConstraints createToInputFieldGBC() {
+        var constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(0, 0, 5, 0);
+        return constraints;
+    }
+
+    private GridBagConstraints createSubjectInputFieldGBC() {
+        var constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(0, 0, 5, 0);
+        return constraints;
+    }
+
+    private GridBagConstraints createMessageContentGBC() {
+        var constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(0, 0, 5, 0);
+        return constraints;
+    }
+
+    private GridBagConstraints createSendButtonGBC() {
+        var constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.weighty = 1;
+        constraints.ipady = 8;
+        return constraints;
+    }
 }
